44b25eaa580dce2ff7fd77c72dc80f2b27a47b33
Ashim Sharma <ashimsharma337@gmail.com>
2023-08-02 14:27:15 -0400

    id generation type channed from auto to identity

--- BackendApi/src/main/java/com/ecom/entities/Product.java
+++ BackendApi/src/main/java/com/ecom/entities/Product.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Table;
 public class Product {
     
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name="Name")
